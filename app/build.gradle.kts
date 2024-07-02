c545d0d726e238def23c58319c4653975c17c297
DagoPeralta94 <dagoperalta163@gmail.com>
2024-07-02 11:33:58 -0500

    Add implementation to dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
+    implementation(project(":data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
